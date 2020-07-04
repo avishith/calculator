cf828919dcd5dcc346ba4a5c814f57fd77cf7673
avishith <pmshithu143@gmail.com>
2020-07-05 01:20:48 +0530

    sin cos tan and log functions added to the calculator as scientific calculator..nav for sci.cal is ready but turn off of sci.cal wants to be fix can use grid_forget()

--- cal.py
+++ cal.py
@@ -11,16 +11,17 @@ import os
 import tkinter as tk
 import  tkinter.ttk as ttk
 import tkinter.messagebox as msg
-import simpleaudio as s
+#import simpleaudio as s
+from math import *
 
-#_______________sounds_______________
+'''#_______________sounds_______________
 welcome = s.WaveObject.from_wave_file("welcome.wav")
 button_sound = s.WaveObject.from_wave_file("buttons.wav")
 math_error = s.WaveObject.from_wave_file("matherror.wav")
 syn_error = s.WaveObject.from_wave_file("syntax_error.wav")
 delete_sound = s.WaveObject.from_wave_file("delete.wav")
 thanks = s.WaveObject.from_wave_file("thanks.wav")
-result = s.WaveObject.from_wave_file("result.wav")
+result = s.WaveObject.from_wave_file("result.wav")'''
 
 
 #ivade modules set cheyyam
@@ -77,12 +78,37 @@ def exit():
 	msg.showwarning("Calculator", "DO YOU REALLY WANTS TO EXIT..?")
 	button_exit.command=cal.destroy
 	thanks.play()
+	#===============scientific calculator==============
+
+def sci():
+	global s
+	
+	if s==1:
+		button_sin=ttk.Button(cal,text="sin",command=lambda:press('sin'))
+		button_sin.grid(row=2,column=0,pady=20)#padx=16,pady=16)
+		
+		button_cos=ttk.Button(cal,text="cos",command=lambda:press("cos"))
+		button_cos.grid(row=2,column=1,pady=20)
+		
+		button_tan=ttk.Button(cal,text="tan",command=lambda:press("tan"))
+		button_tan.grid(row=2,column=2,pady=20)#,padx=16)
+		
+		button_log=ttk.Button(cal,text="log",command=lambda:press("log"))
+		button_log.grid(row=2,column=3,pady=20)#,padx=16)
+		s=0
+	elif a==0:
+		button_log.grid_forget()
+		s=1
+	
+	
+	                
+
 
 #=====Function for clear button======
 
 def clear():
 	global exp
-	delete_sound.play()
+	#delete_sound.play()
 	exp=""
 	text.set(exp)
 	
@@ -90,7 +116,7 @@ def clear():
 	
 def press(num):
 	global exp
-	button_sound.play()
+	#button_sound.play()
 	exp += str(num)
 	text.set(exp)
 	
@@ -99,21 +125,24 @@ def press(num):
 def eql():
         try:
             global exp
-            result.play()
+            #result.play()
             exp=str(eval(exp))
             text.set(exp)
         except ZeroDivisionError:
-                math_error.play()
+                #math_error.play()
                 text.set("Math Error occured...!")
         except SyntaxError:
-                syn_error.play()
+                #syn_error.play()
                 text.set("Syntax Error occured...!")
+        except NameError:
+         	#math_error.play()
+         	text.set('Syntax Error occured....!')
 	
 #========Delet the last degit=========
 
 def delet():
 	global exp
-	delete_sound.play()
+	#delete_sound.play()
 	le=len(exp)
 	exp=exp[0:(le-1)]
 	text.set(exp)
@@ -124,7 +153,7 @@ def exi():
 	
 '''	
 
-welcome.play()
+#welcome.play()
 cal=tk.Tk()
 cal.title("My Python Calculator")
 cal.geometry('360x550')
@@ -132,6 +161,7 @@ cal.resizable(0,0)
 style = ThemedStyle(cal)
 style.set_theme("alt")
 a=0#variable for theme
+s=1
 exp=""
 text=tk.StringVar()
 
@@ -145,9 +175,9 @@ help=ttk.Button(cal,text="HELP",command=help)
 help.grid(row=0,column=1)
 
 mb["menu"] =  mb.menu
-mb.menu.add_checkbutton ( label="SOUND") 
+mb.menu.add_checkbutton ( label="Sound") 
 mb.menu.add_checkbutton ( label="THEME",command=theme) 
-
+mb.menu.add_checkbutton ( label="sci.cal",command=sci)
 
 #( label="Change Theme",command=theme)
 
@@ -159,64 +189,77 @@ entry.focus()
 #===========BUttons===============
 
 
-button_1=ttk.Button(cal,text="1",command=lambda:press('1'))
-button_1.grid(row=2,column=0,pady=20)#padx=16,pady=16)
+button_1=ttk.Button(cal,text="1",width=5,command=lambda:press('1'))
+button_1.grid(row=3,column=0,pady=20)#padx=16,pady=16)
 
 button_2=ttk.Button(cal,text="2",command=lambda:press(2))
-button_2.grid(row=2,column=1,pady=20)
+button_2.grid(row=3,column=1,pady=20)
 
 button_3=ttk.Button(cal,text="3",command=lambda:press(3))
-button_3.grid(row=2,column=2,pady=20)#,padx=16)
+button_3.grid(row=3,column=2,pady=20)#,padx=16)
 
 button_4=ttk.Button(cal,text="4",command=lambda:press('4'))
-button_4.grid(row=3,column=0,pady=20)#,padx=10,pady=10)
+button_4.grid(row=4,column=0,pady=20)#,padx=10,pady=10)
 
 button_5=ttk.Button(cal,text="5",command=lambda:press('5'))
-button_5.grid(row=3,column=1,pady=20)
+button_5.grid(row=4,column=1,pady=20)
 
 button_6=ttk.Button(cal,text="6",command=lambda:press('6'))
-button_6.grid(row=3,column=2,pady=20)
+button_6.grid(row=4,column=2,pady=20)
 
 button_7=ttk.Button(cal,text="7",command=lambda:press('7'))
-button_7.grid(row=4,column=0,pady=20)
+button_7.grid(row=5,column=0,pady=20)
 
 button_8=ttk.Button(cal,text="8",command=lambda:press('8'))
-button_8.grid(row=4,column=1,pady=20)
+button_8.grid(row=5,column=1,pady=20)
 
 
 button_9=ttk.Button(cal,text="9",command=lambda:press('9'))
-button_9.grid(row=4,column=2,pady=20)
+button_9.grid(row=5,column=2,pady=20)
 
 button_0=ttk.Button(cal,text="0",command=lambda:press('0'))
-button_0.grid(row=5,column=1,pady=20)
+button_0.grid(row=6,column=1,pady=20)
+
 
 button_dec=ttk.Button(cal,text=".",command=lambda:press("."))
-button_dec.grid(row=5,column=0,pady=20)
+button_dec.grid(row=6,column=0,pady=20)
 
 button_plus=ttk.Button(cal,text="+",command=lambda:press('+'))
-button_plus.grid(row=2,column=3,pady=20)
+button_plus.grid(row=3,column=3,pady=20)
 
 button_sub=ttk.Button(cal,text="-",command=lambda:press('-'))
-button_sub.grid(row=3,column=3,pady=20)
+button_sub.grid(row=4,column=3,pady=20)
 
 button_mul=ttk.Button(cal,text="*",command=lambda:press('*'))
-button_mul.grid(row=4,column=3,pady=20)
+button_mul.grid(row=5,column=3,pady=20)
 
 button_div=ttk.Button(cal,text="/",command=lambda:press('/'))
-button_div.grid(row=5,column=3,pady=20)
+button_div.grid(row=6,column=3,pady=20)
 
 button_equ=ttk.Button(cal,text="=",command=eql)
-button_equ.grid(row=6,columnspan=4,pady=20,sticky=('nswe'))
+button_equ.grid(row=7,column=1,columnspan=2,pady=20,sticky=('nswe'))
+cal.bind("<Return>",eql)
+
+
 
 
 button_c=ttk.Button(cal,text="del",command=delet)
-button_c.grid(row=7,columnspan=2,sticky=('nswe'),pady=20,padx=20)
+button_c.grid(row=8,columnspan=2,sticky=('nswe'),pady=20,padx=20)
 
 button_exit=ttk.Button(cal,text="exit",command=exit)
-button_exit.grid(row=7,column=2,columnspan=2,sticky=('nswe'),pady=20,padx=20)
+button_exit.grid(row=8,column=2,columnspan=2,sticky=('nswe'),pady=20)
+
+button_open=ttk.Button(cal,text="(",command=lambda:press("("))
+button_open.grid(row=7,column=0,pady=20,padx=20,sticky=('nswe'))
+	
+button_close=ttk.Button(cal,text=")",command=lambda:press(")"))
+button_close.grid(row=7,column=3,pady=20,padx=20,sticky=('nswe'))
+
+
 
 button_clr=ttk.Button(cal,text="C",command=clear)
-button_clr.grid(row=5,column=2,pady=20)
+button_clr.grid(row=6,column=2,pady=20)
+
 cal.mainloop()
 
 #============END=============
